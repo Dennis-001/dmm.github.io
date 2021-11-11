c8727c0a0255a8e690b0fe3c781cf65a215ce2d9
Dennoh <dennismunene84@gmail.com>
2021-11-11 11:22:40 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,122 @@
+<!Doctype html>
+	<html>
+	<head>
+	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Freelancer DMM</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Real Estate Management website by PMAN">
+	
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+	
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+		</head>
+			
+
+		<body>
+			<div id="fh5co-page">
+			<header id="fh5co-header" role="banner">
+    <div class="container">
+      <div class="row">
+        <div class="header-inner">
+          
+          <a href="index.php"  class="logo">
+            
+          <img src="images/free.png" alt="IMG-LOGO">
+          
+          </a>
+          
+          <nav role="navigation">
+            <ul>
+              <li><a href="index.php">Home</a></li>
+              
+              <li class="cta"><a href="contact.php">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="fh5co-contact animate-box">
+		<div class="container">
+			<div class="row">
+  	<div class="col-md-8 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+  	<div class="row">
+  <form method="post" action="processes/mailing_fxn.php"
+						<div class="col-md-6">
+							<div class="form-group">
+								<input class="form-control" placeholder="Name" type="text" name="name">
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<input class="form-control" placeholder="Email" type="text" name="email">
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="form-group">
+								<input class="form-control" placeholder="Subject" type="text" name="subject">
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="form-group">
+								<textarea class="form-control" id="" cols="30" rows="7" placeholder="Message" name="message"></textarea>
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="form-group">
+								<input value="Send Message" class="btn btn-primary" type="submit" name="contact">
+							</div>
+						</div>
+					</form>
+  	</div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+</body>
+		<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+	</html>
---
+++ index.php
@@ -0,0 +1,120 @@
+<!Doctype html>
+	<html>
+	<head>
+	
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Freelancer DMM</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Real Estate Management website by PMAN">
+	
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> 
+	
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- lang style -->
+	<link rel="stylesheet" href="css/figure.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+		</head>
+			
+
+		<body>
+			<div id="fh5co-page">
+			<header id="fh5co-header" role="banner">
+    <div class="container">
+      <div class="row">
+        <div class="header-inner">
+          
+          <a href="index.php"  class="logo">
+            
+          <img src="images/free.png" alt="IMG-LOGO">
+          
+          </a>
+          
+          <nav role="navigation">
+            <ul>
+              <li><a href="index.php">Home</a></li>
+              
+              <li class="cta"><a href="contact.php">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+		<div id="fh5co-agents">
+		<div class="container">
+			
+				
+				<div class="col-md-4 text-center item-block animate-box" data-animate-effect="fadeIn">
+
+					<div class="fh5co-agent">
+						<figure>
+							<img src="images/photo_2021-11-05_11-29-58.jpg" alt="Free Website Template by FreeHTML5.co">
+						</figure>
+						<h3>Munene Mwenda, Freelancer</h3>
+						<p>The is the kind of freelancer with high regard for professionalism and punctuality. Heavily dedicated to the development of systems, works with ateam of other freelancers to provide the best product. </p>
+						<p><a href="contact.php" class="btn btn-primary btn-outline">Contact Me</a></p>
+						
+					</div>
+				</div>
+				
+		</div>
+	<div id="langc">
+			
+						<div class="lang">	
+							<img  src="images/html.png" alt="Free Website Template by FreeHTML5.co">
+						
+							<img  src="images/css.png" alt="Free Website Template by FreeHTML5.co">
+						
+							<img  src="images/php.png" alt="Free Website Template by FreeHTML5.co">
+						
+							<img  src="images/laravel.png" alt="Free Website Template by FreeHTML5.co">
+						
+					</div>
+					</div>
+	</div>
+	
+
+					
+</div>
+	</body>
+		<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+	</html>
